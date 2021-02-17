edc03ac6432b27997f2e5bff76131fda38213630
Gabriel Cabral <czar.cabral@gmail.com>
2021-02-17 01:49:57 -0800

    set up main page with styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { MainService } from './main.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'pathrise-app-angular';
+export class AppComponent implements OnInit {
+
+  jobBoards: any[] = [];
+
+  constructor(private mainService: MainService) { }
+
+  ngOnInit() {
+    this.mainService.getJobBoards().subscribe(response => {
+      if (response.data) {
+        this.jobBoards = response.data;
+      }
+    }, err => {
+      console.log(err);
+    });
+  }
+
 }
